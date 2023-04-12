3d6f7f2b891bf9624703708019a0e53c5379076d
Michael Nyasera <117773134+MichaelN-SE@users.noreply.github.com>
2023-04-12 23:19:04 +0300

    Create 0-read_file.py
    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Reads into text file"""
+
+
+def read_file(filename=""):
+    """open file using with"""
+
+    with open(filename, mode='r', encoding='utf-8') as a_file:
+        print(a_file.read(), end='')
