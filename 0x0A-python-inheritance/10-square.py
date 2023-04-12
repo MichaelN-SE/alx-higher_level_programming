8d8f8a365362e70d6e924eed8f1c6d73a4ce6f52
Michael Nyasera <117773134+MichaelN-SE@users.noreply.github.com>
2023-04-12 22:59:07 +0300

    Create 10-square.py
    Square#1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Inherits from a rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class inherit from a class that inherited from another"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+
+    def area(self):
+        return self.__size ** 2
+
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
