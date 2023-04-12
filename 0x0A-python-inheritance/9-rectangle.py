c5ce39417ab8a57445e09136bab9c686d77ceb21
Michael Nyasera <117773134+MichaelN-SE@users.noreply.github.com>
2023-04-12 22:49:31 +0300

    Create 9-rectangle.py
    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Contains the class BaseGeometry and subclass Rectangle """
+
+BaseGeometry  = __import__('7-base_geometry').BaseGeometry
+"""importing module with class to inherit"""
+
+
+class Rectangle(BaseGeometry):
+    """define with and height"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        me = "{}".format(self.__class__.__name__)
+        return "[{}] {}/{}".format(me, self.__width, self.__height)
