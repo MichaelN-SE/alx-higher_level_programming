7853afe8e2bc3e1bb47cbff72871b443de5d5a92
Michael Nyasera <117773134+MichaelN-SE@users.noreply.github.com>
2023-04-12 23:04:53 +0300

    Create 100-my_int.py
    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class Override  ==  operator with !="""
+
+class MyInt(int):
+    """Override  ==  operator and !="""
+
+    def __eq__(self, b):
+
+        return super().__ne__(b)
+
+    def __ne__(self, b):
+        return super().__eq__(b)
