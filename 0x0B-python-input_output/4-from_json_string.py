821ca877ca0b45cb4b8612ad6dbdda9211ac908a
Michael Nyasera <117773134+MichaelN-SE@users.noreply.github.com>
2023-04-12 23:34:19 +0300

    Create 4-from_json_string.py
    From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""decoding from JSON to python"""
+import  json
+
+
+def from_json_string(my_str):
+    """Returns json string decoded to python"""
+    return json.loads(my_str)
